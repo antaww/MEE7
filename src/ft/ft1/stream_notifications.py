fc13fde49037e09b6d86246650874d574aa92e74
anta <anta.pizzetta@gmail.com>
2024-06-18 01:23:27 +0200

    feat(twitch): Rework message into embed with more information.

--- src/ft/ft1/stream_notifications.py
+++ src/ft/ft1/stream_notifications.py
@@ -1,4 +1,6 @@
 import os
+
+import discord
 import requests
 from discord.ext import tasks
 from dotenv import load_dotenv
@@ -49,10 +51,11 @@ def get_access_token(client_id, client_secret):
 TWITCH_ACCESS_TOKEN = get_access_token(TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET)
 
 # List of streamers to check
-STREAMERS = ["nikof", "alphacast", "fugu_fps"]
+STREAMERS = settings.get('streamers_list')
 
 # Twitch API URL
-API_URL = 'https://api.twitch.tv/helix/streams'
+STREAMS_API_URL = 'https://api.twitch.tv/helix/streams'
+USER_API_URL = 'https://api.twitch.tv/helix/users'
 
 # Twitch API headers
 API_HEADERS = {
@@ -82,62 +85,83 @@ async def check_streamers(bot):
     """
     print("Checking streamers...")
     for streamer in STREAMERS:
-        is_online = await check_user(streamer)  # Check if the streamer is online by calling the `check_user` function.
+        datas = await check_user_and_get_info(
+            streamer)  # Check if the streamer is online by calling the `check_user` function.
+        is_online = bool(datas)
+        # debug
+        # print(f"{streamer} is online: {is_online} : {datas}")
         if is_online and not streamers_status[streamer]:  # If the streamer is online and was previously not online.
             streamers_status[streamer] = True  # Update the streamer's status to online.
-            await notify_discord(streamer, bot)  # Send a notification to Discord by calling the `notify_discord` function.
+            await notify_discord(datas, bot)  # Send a notification to Discord by calling the `notify_discord` function.
         elif not is_online and streamers_status[streamer]:  # If the streamer is not online and was previously online.
             streamers_status[streamer] = False  # Update the streamer's status to offline.
 
 
-async def check_user(streamer):
+async def check_user_and_get_info(streamer):
     """
-    This function checks if a given streamer is currently online on Twitch.
+    This function checks if a given streamer is currently online on Twitch and if so, retrieves the user's information.
 
-    It takes one argument:
-    - streamer: The username of the streamer to check.
+    Args:
+        streamer (str): The username of the streamer to check.
 
-    The function first constructs the URL for the Twitch API endpoint to get the stream information for the streamer.
-    It then sends a GET request to the Twitch API endpoint with the API headers.
+    Returns:
+        dict: A dictionary with the streamer's online status and user information if the streamer is online.
+              If the streamer is not online, it returns an empty dictionary.
 
-    If the request is successful, it retrieves the data from the response and checks if there is any data.
-    If there is data, it means the streamer is online and the function returns True.
-    If there is no data, it means the streamer is not online and the function returns False.
-
-    If the request is not successful or an exception occurs, it prints an error message to the console and returns False.
-
-    This function returns a boolean indicating whether the streamer is online or not.
+    The function first checks if the streamer is online by sending a GET request to the Twitch API endpoint.
+    If there is any data in the response, the streamer is considered online and the function proceeds to retrieve the user's information.
+    The user's information is retrieved by sending another GET request to the Twitch API endpoint, this time with the user_id obtained from the streamer's data.
+    If there is any data in the response, the user's information is added to the return dictionary along with the streamer's data.
+    If an exception occurs during the process, an error message is printed and an empty dictionary is returned.
     """
-    url = f'{API_URL}?user_login={streamer}'  # Construct the URL for the Twitch API endpoint.
+    url = f'{STREAMS_API_URL}?user_login={streamer}'  # Construct the URL for the Twitch API endpoint.
     try:
-        req = requests.get(url, headers=API_HEADERS)  # Send a GET request to the Twitch API endpoint with the API headers.
+        req = requests.get(url,
+                           headers=API_HEADERS)  # Send a GET request to the Twitch API endpoint with the API headers.
         json_data = req.json()  # Retrieve the data from the response.
         if json_data.get('data'):  # If there is any data, the streamer is online.
-            return True
-        else:
-            return False
+            streamer_data = json_data['data'][0]
+
+            # Get user info
+            user_id = streamer_data['user_id']
+            url = f'{USER_API_URL}?id={user_id}'
+            req = requests.get(url, headers=API_HEADERS)
+            json_data = req.json()
+            if json_data.get('data'):
+                user_info = json_data['data'][0]
+                return {'streamer_data': streamer_data, 'user_info': user_info}
     except Exception as e:
-        print("Error checking user:", e)
-        return False
+        print("Error checking user or getting user info:", e)
+    return {}
 
 
-async def notify_discord(streamer, bot):
+async def notify_discord(datas, bot):
     """
     This function sends a notification to a specific Discord channel when a streamer starts streaming on Twitch.
 
-    It takes two arguments:
-    - streamer: The username of the streamer who started streaming.
-    - bot: The bot instance.
+    Args:
+        datas (dict): A dictionary containing the streamer's data and user information.
+        bot (discord.Client): The bot instance.
 
-    The function first retrieves the bot channel using the channel_id.
-    If the bot channel exists, it sends a message to the channel with the streamer's name and a link to their Twitch stream.
+    The function first retrieves the bot channel using the channel_id from the settings. If the bot channel exists,
+    it creates an embed message with the streamer's name, game, stream title, profile image, and a link to their
+    Twitch stream. The embed message is then sent to the bot channel.
 
     This function doesn't return anything.
     """
     channel_id = settings.get('twitch_channel_id')  # The ID of the Discord channel to send the notification to.
     bot_channel = bot.get_channel(channel_id)
     if bot_channel:  # If the bot channel exists.
-        await bot_channel.send(
-            f"> :tada: **{streamer}** started streaming on Twitch!\n"
-            f"> :arrow_right_hook: Watch here: https://www.twitch.tv/{streamer}"
+        streamer_data = datas['streamer_data']
+        user_info = datas['user_info']
+        embed = discord.Embed(
+            title=f":tada: {user_info['display_name']} started streaming on Twitch!",
+            description=streamer_data['title'],
+            color=0x9146ff
         )
+        embed.add_field(name="Game", value=streamer_data['game_name'], inline=True)
+        embed.add_field(name=":arrow_double_down:", value=f"[Watch here !](https://www.twitch.tv/{user_info['login']})", inline=True)
+        embed.set_thumbnail(url=user_info['profile_image_url'])
+        embed.set_footer(text="MEE7 Twitch Stream Notifications",
+                         icon_url="https://i.imgur.com/n3LnWW7.png")
+        await bot_channel.send(embed=embed)
